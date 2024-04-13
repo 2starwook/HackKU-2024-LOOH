7322cfa57f4d6cea4e10d15b7e31316911ec9a3a
leeable <yiwoduf@gmail.com>
2024-04-13 14:30:14 -0500

    NextJS Connection with Flask

---
+++ app/components/component.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+function Component() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5328/")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.text();
+      })
+      .then((data) => {
+        setMessage(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setMessage("Error fetching data");
+      });
+  }, []);
+
+  return (
+    <div>
+      <p>API로부터 받은 메시지:</p>
+      <div dangerouslySetInnerHTML={{ __html: message }}></div>
+    </div>
+  );
+}
+
+export default Component;
--- main.py
+++
@@ -1,7 +0,0 @@
-from backend import create_app
-
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(port=8000)
