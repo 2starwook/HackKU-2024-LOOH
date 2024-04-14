70daa3bbc693d19d21c43545e7ce2545b126b7ff
leeable <yiwoduf@gmail.com>
2024-04-13 19:25:23 -0500

    Landing Page Update

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,18 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-50 dark:bg-gray-800">
       <Component />
-      <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by clicking&nbsp;
-          <Link href="/form">
-            <code className="font-mono font-bold">HERE</code>
-          </Link>
+      <div className="max-w-5xl flex lg:flex-col items-center font-mono text-sm lg:flex-col">
+        <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-4 mt-4">
+          ㅎㅇ
+        </h1>
+        <p className="mt-4 text-lg text-gray-600 dark:text-gray-300 mb-4">
+          디스크립션
         </p>
+        <Link href="/form" passHref>
+          <span className="mt-2 cursor-pointer">GOGOGOGO</span>
+        </Link>
       </div>
     </main>
   );
--- components/component.tsx
+++ components/component.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import "../app/globals.css";
+
 import React, { useEffect, useState } from "react";
 
 function Component() {
