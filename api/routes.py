73053c6f699dd278e4bf8d2b2f118f4548709a57
Isaac Lee <2starwook@gmail.com>
2024-04-13 20:37:13 -0500

    Handle ValueError in predict method

--- api/routes.py
+++ api/routes.py
@@ -2,6 +2,7 @@ import numpy as np
 from flask import Blueprint, request, jsonify
 from api import get_model
 # from sklearn.linear_model import RidgeCV
+# from api.config import App_config
 
 
 routes_bp = Blueprint('routes', __name__, url_prefix='/')
@@ -29,15 +30,19 @@ def predict():
     # model: RidgeCV
     if "model" not in globals():
         model = get_model()
-    bedrooms = request.args.get('bedrooms', type=int)
-    bathrooms = request.args.get('bathrooms', type=int)
-    stories = request.args.get('stories', type=int)
-    mainroad = request.args.get('mainroad', type=int)
-    guestroom = request.args.get('guestroom', type=int)
-    basement = request.args.get('basement', type=int)
-    hotwaterheating = request.args.get('hotwaterheating', type=int)
-    airconditioning = request.args.get('airconditioning', type=int)
-    parking = request.args.get('parking', type=int)
+    try:
+        bedrooms = request.args.get('bedrooms', type=int)
+        bathrooms = request.args.get('bathrooms', type=int)
+        stories = request.args.get('stories', type=int)
+        mainroad = request.args.get('mainroad', type=int)
+        guestroom = request.args.get('guestroom', type=int)
+        basement = request.args.get('basement', type=int)
+        hotwaterheating = request.args.get('hotwaterheating', type=int)
+        airconditioning = request.args.get('airconditioning', type=int)
+        parking = request.args.get('parking', type=int)
+    except ValueError:
+        # TODO - Handle ValueError and notify to client
+        pass
     predicted_price = model.predict(np.reshape([
         bedrooms, bathrooms, stories, mainroad, mainroad, guestroom, basement, 
         hotwaterheating, airconditioning, parking
